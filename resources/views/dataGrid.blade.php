bd3ddb49c9660673751d655f5f80a648ce9d7a25
Buena Mar Erracho <buenamarerracho97@gmail.com>
2023-07-10 03:56:59 +0800

    - remove code

--- resources/views/dataGrid.blade.php
+++ resources/views/dataGrid.blade.php
@@ -82,14 +82,6 @@
             }
             retrieveKangarooInfo();
 
-            // $('#positionSorting').dxCheckBox({
-            //     value: false,
-            //     text: 'Disable Sorting for the Position Column',
-            //     onValueChanged(data) {
-            //     dataGrid.columnOption(5, 'sortOrder', undefined);
-            //     dataGrid.columnOption(5, 'allowSorting', !data.value);
-            //     },
-            // });
         });
 
     </script>
